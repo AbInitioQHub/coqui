88b9bc739dd01f3c58d605c24bc8013828718a12
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2026-09-04 00:48:02 +0000

    Allow running embed with different iterations of downfold_1e and downfold_2e for qsGW+DMFT

--- src/methods/embedding/embed_t.cpp
+++ src/methods/embedding/embed_t.cpp
@@ -90,11 +90,13 @@ namespace methods {
 
     _Timer.start("EMBED_READ");
     auto [gw_iter, weiss_f_iter, weiss_b_iter, embed_iter] = chkpt::read_input_iterations(filename);
-    utils::check(weiss_f_iter == weiss_b_iter,
-                 "embed_t::dmft_embed_impl: inconsistent downfolding iterations "
-                 "for fermionic ({}) and bosonic ({}) Weiss fields in {}",
-                 weiss_f_iter, weiss_b_iter, filename);
-    long embed_out_iter = (embed_iter > 0) ? embed_iter+1 : 1;
+
+    utils::check(weiss_b_iter > 0,
+                 "embed_t::dmft_embed_impl: weiss_b_iter <= 0, indicating "
+                 "no effective Coulomb interactions found in {}. "
+                 "Please run \"downfold_2e\" first. ", filename);
+
+    long embed_out_iter = (embed_iter > 0) ? embed_iter+1 : weiss_f_iter;
     _Timer.stop("EMBED_READ");
 
     ft.metadata_log();
